41c71e918be3b31ebb2e64b3fbf03aa34662d360
DIEGO <diegomarta14@gmail.com>
2023-04-27 02:53:26 +0200

    Imaganes servidor bien

--- app/src/main/java/com/example/proyectodas2/ServicioFirebase.java
+++ app/src/main/java/com/example/proyectodas2/ServicioFirebase.java
@@ -9,6 +9,8 @@ import androidx.work.ListenableWorker;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
+import org.json.JSONObject;
+
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -29,52 +31,50 @@ public class ServicioFirebase extends Worker {
         // Recojo el token
         String token = getInputData().getString("token");
 
-        String direccion = "http://ec2-54-93-62-124.eu-central-1.compute.amazonaws.com/dmarta002/WEB/Firebase.php";
-        // Conexion con el servidor
+        String dir = "http://ec2-54-93-62-124.eu-central-1.compute.amazonaws.com/dmarta002/WEB/Firebase.php";
         HttpURLConnection urlConnection = null;
+
+        String usuario = getInputData ().getString ("usuario");
+        String pass = getInputData ().getString ("contrasena");
+
+
         try {
-            URL destino = new URL(direccion);
-            urlConnection = (HttpURLConnection) destino.openConnection();
-            urlConnection.setReadTimeout(5000);
+            URL dest = new URL(dir);
+            urlConnection = (HttpURLConnection) dest.openConnection();
             urlConnection.setConnectTimeout(5000);
-
-
-            // Se envian los parametros
-            String parametros = "token=" + token;
-
-            Log.d("conexion", "Parametros fcm: " + parametros);
-
+            urlConnection.setReadTimeout(5000);
             urlConnection.setRequestMethod("POST");
-            urlConnection.setDoOutput(true);
-            urlConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            urlConnection.setRequestProperty("Content-Type", "application/json");
+            JSONObject paramJson = new JSONObject();
+            paramJson.put("token", token);
+
+
             PrintWriter out = new PrintWriter(urlConnection.getOutputStream());
-            out.print(parametros);
-            out.flush();
-
-            // Se obtiene la respuesta
-            int status = urlConnection.getResponseCode();
-            Log.d("conexion", "Codigo Respuesta " + status);
-            Log.d("conexion", "Respuesta:" + urlConnection.getResponseMessage());
-
-
-            // En caso de obtener 200
-            if (status == 200) {
+            out.print(paramJson.toString());
+            out.close();
+            int statusCode = urlConnection.getResponseCode();
+            Log.d("Prueba","Titooos"+paramJson);
+            Log.d("respuesta","Titooos"+statusCode);
+            if (statusCode == 200) {
                 BufferedInputStream inputStream = new BufferedInputStream(urlConnection.getInputStream());
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
-                String line, result = "";
+                String line;
+                StringBuilder result = new StringBuilder();
+                Log.d("respuesta","Titooos ha entrado2");
                 while ((line = bufferedReader.readLine()) != null) {
-                    result += line;
+                    result.append(line);
+                    Log.d("respuesta","Titooos ha entrado3");
                 }
+                Log.d("respuesta","Titooos: "+result);
                 inputStream.close();
+                Log.d("respuesta","Titooos ha entrado5");
+                boolean exito = result.toString().equals("true");
 
-                Data data = new Data.Builder()
-                        .putString("result", result)
-                        .build();
-                // Se devuelve la respuesta
-                return ListenableWorker.Result.success(data);
+                Data.Builder b = new Data.Builder();
+                return Result.success(b.putBoolean("result", exito).build());
             }
         } catch (Exception e) {
-            Log.d("conexion", "Error: " + e);
+            Log.e ("EXCEPTION", "doWork: ", e);
         }
         return ListenableWorker.Result.failure();
     }
